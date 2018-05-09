12ac8e191bc003c3060e1a508db197dc785687e8
Adam Nemecek <adamnemecek@gmail.com>
2018-05-09 10:02:39 -0700

    Trimmed whitespace.

--- ModeAnalysisGUI/MetalRender/Mesh.swift
+++ ModeAnalysisGUI/MetalRender/Mesh.swift
@@ -20,11 +20,11 @@ public class Mesh {
         case FrameUniform = 1
         func index() -> Int {return self.rawValue}
     }
-    
+
     let vertexBuffer: MTLBuffer
     let indexBuffer: MTLBuffer
     let indexCount:Int
-    
+
     public init(pos: [double3], normal: [double3] = [], indices: [[Int]], color: [double4] = [], values: [double_t] = []) {
         var vertexArray:[VertexStructure] = []
         var indexArray:[uint32] = []
@@ -38,14 +38,14 @@ public class Mesh {
             let vertexAttrib = VertexStructure(pos: posf, normal: norf, color: colf, value: valf)
             vertexArray.append(vertexAttrib)
         }
-        
+
         indices.forEach { (a) in
               a.forEach({ (b) in
                     indexArray.append(uint32(b))
               })
         }
         indexCount = indexArray.count
-        
+
         guard let device = Render.current.device else {
             fatalError("Render has not been initialized.")
         }
@@ -56,14 +56,14 @@ public class Mesh {
         }
         vertexBuffer = vBuff
         indexBuffer = idBuff
-        
+
         if vertexArray.count-1 != Int(indexArray.max()!) {
             NSLog("vertexcount \(vertexArray.count-1) doesn't match max of indexArray \(String(describing: indexArray.max()))")
         }
     }
 
-    
-    
+
+
     func render(encoder :MTLRenderCommandEncoder) {
         encoder.setVertexBuffer(vertexBuffer, offset: 0, index: Buffer.MeshVertex.index())
         encoder.drawIndexedPrimitives(type: .triangle, indexCount: indexCount, indexType: .uint32, indexBuffer: indexBuffer, indexBufferOffset: 0)
--- ModeAnalysisGUI/Model/Primitives.swift
+++ ModeAnalysisGUI/Model/Primitives.swift
@@ -14,12 +14,12 @@ class Parallelogram: Polygon {
                 var p0 = double3(dx*double_t(j),dy*double_t(i),0)-double3(0.5,0.5,0)
                 p0.x += p0.y
                 vertices.append(p0)
-                
+
                 normal.append(double3(0,0,1))
                 displacementBases.append(p0)
             }
         }
-        
+
         for i in 0..<col-1 {
             for j in 0..<row-1 {
                 faces.append([j+i*row,j+(i+1)*row,j+1+i*row])
@@ -33,48 +33,48 @@ class Sphere: Polygon {
     override func setMesh() {
         let row:Int = 30
         let col:Int = 30
-        
+
         let r:double_t = 1
-        
+
         vertices.append(double3(0,0,-1))
-        
+
         for i in 1..<col {
             var th = (1-double_t(i)/double_t(col))*double_t.pi
             th /= 1-1/double_t(col)
             th -= double_t.pi/double_t(col)*0.5
-            
-            
+
+
             for j in 0..<row {
                 let phi = double_t(j)/double_t(row)*double_t.pi*2
                 let x:double_t = r*sin(th)*cos(phi)
                 let y:double_t = r*sin(th)*sin(phi)
                 let z:double_t = r*cos(th)
                 vertices.append(double3(x,y,z))
-                
+
             }
         }
         vertices.append(double3(0,0,1))
-        
+
         let offset:Int = vertices.count-row-1
         for i in 0..<row {
             faces.append([0,i+1,(i+1)%row+1])
             faces.append([vertices.count-1,(i+1)%row+offset,i+offset])
         }
-        
+
         for i in 0..<col-1-1 {//謎の-2
             for j in 0..<row {
                 faces.append([j+i*row+1,j+(i+1)*row+1,(j+1)%row+i*row+1])
                 faces.append([j+(i+1)*row+1,(j+1)%row+(i+1)*row+1,(j+1)%row+i*row+1])
             }
         }
-        
+
         for i in 0..<faces.count {
             let face:[Int] = faces[i]
             let u:double3 = vertices[face[1]]-vertices[face[0]]
             let v:double3 = vertices[face[2]]-vertices[face[0]]
             let crs:double3 = cross(u, v)
             let p:double3 = vertices[face[0]]
-            
+
             if dot(crs, p)<0.0 {
                 let v1:Int = faces[i][1]
                 let v2:Int = faces[i][2]
@@ -82,7 +82,7 @@ class Sphere: Polygon {
                 faces[i][2] = v1
             }
         }
-        
+
         normal = vertices.map{return normalize($0)}
         displacementBases = Array(repeating: double3(0), count: vertices.count)
     }
@@ -94,7 +94,7 @@ class Torus: Polygon {
         let col:Int = 60
         let R:double_t = 1
         let r:double_t = 0.5
-        
+
         for i in 0..<col {
             let th = double_t(i)/double_t(col)*double_t.pi*2
             let p0 = double3(R*cos(th),R*sin(th),0)
@@ -108,10 +108,10 @@ class Torus: Polygon {
                 displacementBases.append(p0)
             }
         }
-        
+
         for i in 0..<col {
             for j in 0..<row {
-                
+
                 faces.append([j+i*row,j+((i+1)%col)*row,(j+1)%row+i*row])
                 faces.append([j+((i+1)%col)*row,(j+1)%row+((i+1)%col)*row,(j+1)%row+i*row])
             }
@@ -133,12 +133,12 @@ class GeodesicDome: Polygon {
             }
         }
         var edge:[[Int]] = [[Int]()]
-        
+
         let itr:Int = 5
         for k in 0..<itr {
             let n = vertex.count
             edge =  Array(repeating: Array(repeating: 0, count: n), count: n)
-            
+
             var vertex_temp = vertex
             for p in vertex {
                 //edge探索
@@ -156,9 +156,9 @@ class GeodesicDome: Polygon {
                     edge[vertex_temp[6].1][p.1] = 1
                 }
             }
-            
+
             if k==itr-1 {break}
-            
+
             //頂点追加
             var id:Int = n
             for i in 0..<n {
@@ -172,14 +172,14 @@ class GeodesicDome: Polygon {
                     }
                 }
             }
-            
+
             print("present vertices count \(vertex.count)")
         }
-        
-        
-        
+
+
+
         var face:[[Int]] = [[Int]]()
-        
+
         for i in 0..<vertex.count {
             for j in i+1..<vertex.count where edge[i][j]==1 {
                 for k in 0..<vertex.count {
@@ -192,21 +192,21 @@ class GeodesicDome: Polygon {
                         }else{
                             face.append([i,k,j])
                         }
-                        
+
                     }
                     edge[i][j] = 0
                     edge[j][i] = 0
                 }
             }
         }
-        
+
         for (p,_) in vertex {
             vertices.append(p)
             normal.append(normalize(p))
             displacementBases.append(double3(0))
         }
         faces = face
-        
+
     }
 }
 
@@ -218,7 +218,7 @@ class SphereImplicit: Polygon {
         }
         let dual = DualContouring3D(row: 40, edgeLength: 3, distance: distance)
         let (dualVertex,face) = dual.makePolygonsWithEdges()
-        
+
         faces = face
         dualVertex.forEach { (v) in
             let p = double3(Double(v.pos.x),Double(v.pos.y),Double(v.pos.z))
@@ -227,7 +227,7 @@ class SphereImplicit: Polygon {
             normal.append(n)
             displacementBases.append(double3(0))
         }
-        
+
         for i in 0..<faces.count {
             let face = faces[i]
             let u:double3 = vertices[face[1]]-vertices[face[0]]
@@ -236,7 +236,7 @@ class SphereImplicit: Polygon {
                 (faces[i][1],faces[i][2]) = (faces[i][2],faces[i][1])
             }
         }
-        
+
     }
 }
 
@@ -245,10 +245,10 @@ class Mobius: Polygon {
     override func setMesh() {
         let row:Int = 10
         let col:Int = 100//circle
-        
+
         let radius:double_t = 1
         let width:double_t = 0.2
-        
+
         for i in 0..<col {
             let th = double_t(i)/double_t(col)*double_t.pi*2
             let p0 = radius*double3(cos(th),sin(th),0)
@@ -258,21 +258,21 @@ class Mobius: Polygon {
                 let p1:double3 = double_t(j*2-row+1)/double_t(row)*width*u
                 let p:double3 = p0+p1
                 vertices.append(p)
-                
+
                 let v:double3 = normalize(double3(-p0.y,p0.x,0))
                 let n:double3 = normalize(cross(u, v))
                 normal.append(n)
                 displacementBases.append(p)
             }
         }
-        
+
         for i in 0..<col-1 {
             for j in 0..<row-1 {
                 faces.append([j+i*row,j+(i+1)*row,j+1+i*row])
                 faces.append([j+(i+1)*row,j+1+(i+1)*row,j+1+i*row])
             }
         }
-        
+
         //FIXME: このままではpairが登録されないので境界として認識されてしまう
         for j in 0..<row-1 {
             faces.append([j+(col-1)*row,row-1-j+0,j+1+(col-1)*row])
